809e3fd6eaabbd201c7c82885ebd9ac906637c22
Siddharth Paneri <siddharth.udr@gmail.com>
2019-06-21 22:23:37 +0530

    Ad function created

---
+++ Calculator.py
@@ -0,0 +1,4 @@
+class Calculator:
+	def add(self, a, b) :
+		sum_value = a+b
+		return sum_value
---
+++ User.py
@@ -0,0 +1,4 @@
+from Calculator import Calculator
+calc = Calculator()
+value = calc.add(1, 5)
+print(value)
